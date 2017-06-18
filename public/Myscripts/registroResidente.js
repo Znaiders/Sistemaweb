16405be731a1d00d62f895a6922e27134b5ec692
znaiders <esnaiderivan@gmail.com>
2017-06-18 18:29:05 -0500

    Separacion del Script para el RegistroResidente

---
+++ public/Myscripts/registroResidente.js
@@ -0,0 +1,23 @@
+function mostrarResidente(){
+document.getElementById('residente').style.display = 'block';
+document.getElementById('empresa').style.display = 'none';
+document.getElementById('proyecto').style.display = 'none';
+
+}
+
+
+function mostrarEmpresa(){
+document.getElementById('empresa').style.display = 'block';
+document.getElementById('residente').style.display = 'none';
+document.getElementById('proyecto').style.display = 'none';
+
+
+}
+
+function mostrarProyecto(){
+document.getElementById('proyecto').style.display = 'block';
+document.getElementById('residente').style.display = 'none';
+document.getElementById('empresa').style.display = 'none';
+}
+/*alert("Estoy aqui");
+console.log("Como va la nueva funcion?");*/
