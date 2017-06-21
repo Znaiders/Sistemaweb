7e44b7c15a7d85263a84d66f0410cebee5256b50
znaiders <esnaiderivan@gmail.com>
2017-06-21 01:13:05 -0500

    ScriptModficado Hol

---
+++ public/Myscripts/hola.js
@@ -0,0 +1,21 @@
+function mostrarResidente(){
+document.getElementById('residente').style.display = 'block';
+document.getElementById('empresa').style.display = 'none';
+document.getElementById('proyecto').style.display = 'none';
+
+}
+
+
+function mostrarEmpresa(){
+document.getElementById('empresa').style.display = 'block';
+document.getElementById('residente').style.display = 'none';
+document.getElementById('proyecto').style.display = 'none';
+
+
+}
+
+function mostrarProyecto(){
+document.getElementById('proyecto').style.display = 'block';
+document.getElementById('residente').style.display = 'none';
+document.getElementById('empresa').style.display = 'none';
+}
